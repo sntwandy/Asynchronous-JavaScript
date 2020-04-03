e44b150b16a1f1e41bb0a2399e3852f4cd7b97e2
sntwandy <sntwandy@gmail.com>
2020-04-03 11:18:51 -0400

    Finishing using callbacks and XMLHttpRequest to fetch data from the API

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,6 +1,7 @@
-// Import dependencies 'XMLHttpRequest'.
-const XMLHttpRequest = import('xmlhttprequest').XMLHttpRequest;
+// Require dependencies 'XMLHttpRequest'.
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+const API = 'https://rickandmortyapi.com/api/character/';
 // Create a Fn to fetch the data using 'XMLHttpRequest'.
 function fetchData(urlApi, callback){
     // Create 'XMLHttpRequest' instance = 'xhttp'.
@@ -8,7 +9,7 @@ function fetchData(urlApi, callback){
     // Create the petition to the API, with 'open', parameters: 'GET', urlAPI, true (activate the asynchronous, by default is 'true').
     xhttp.open('GET', urlApi, true);
     // Now, we check the response state.
-    xhttp.onreadystatechange = (event) =>{
+    xhttp.onreadystatechange = function(event) {
         // Check is the 'readyState' is = 4 (Petition to the API is completed).
         if(xhttp.readyState === 4){
             // Check is the status is = 200 (Petition fetch data was success!.)
@@ -25,3 +26,17 @@ function fetchData(urlApi, callback){
     // We use the Fn 'send'.
     xhttp.send();
 }
+
+// Execution the callbacks (hell)
+fetchData(API, function(error1, data1){
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if(error2) console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3){
+            if(error3) console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
